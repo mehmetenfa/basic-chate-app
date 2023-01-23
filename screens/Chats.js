639331707846817dd9fe7e7bdd2579e73a00836b
qxan <mehmet.enfa@gmail.com>
2023-01-23 22:19:23 +0300

    add screens folder

---
+++ screens/Chats.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Chats = () => {
+  return (
+    <View>
+      <Text>Hi, Chats Screen!</Text>
+    </View>
+  );
+};
+
+export default Chats;

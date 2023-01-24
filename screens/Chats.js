7a4563c38dfb20079aae4ff117f8eed113e6c54a
qxan <mehmet.enfa@gmail.com>
2023-01-24 20:32:00 +0300

    add arrow icon

--- screens/Chats.js
+++ screens/Chats.js
@@ -1,29 +1,36 @@
 import React from "react";
 import { Text, SafeAreaView, TouchableOpacity, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 const Chats = () => {
   return (
     <SafeAreaView>
       <TouchableOpacity style={styles.row}>
-        <Text style={styles.name}>User Name</Text>
-        <Text style={styles.subtitle}>Hi, I am waiting for you for dinner</Text>
+        <View style={styles.textsContainer}>
+          <Text style={styles.name}>User Name</Text>
+          <Text style={styles.subtitle}>
+            Hi, I am waiting for you for dinner
+          </Text>
+        </View>
+
+        <Ionicons size={20} name="chevron-forward-outline" />
       </TouchableOpacity>
 
-      <View style={styles.serprator}/>
+      <View style={styles.serprator} />
 
       <TouchableOpacity style={styles.row}>
         <Text style={styles.name}>User Name</Text>
         <Text style={styles.subtitle}>Hi, I am waiting for you for dinner</Text>
       </TouchableOpacity>
 
-      <View style={styles.serprator}/>
+      <View style={styles.serprator} />
 
       <TouchableOpacity style={styles.row}>
         <Text style={styles.name}>User Name</Text>
         <Text style={styles.subtitle}>Hi, I am waiting for you for dinner</Text>
       </TouchableOpacity>
 
-      <View style={styles.serprator}/>
+      <View style={styles.serprator} />
 
       <TouchableOpacity style={styles.row}>
         <Text style={styles.name}>User Name</Text>
@@ -34,22 +41,28 @@ const Chats = () => {
 };
 
 const styles = StyleSheet.create({
-  row:{
+  row: {
     paddingHorizontal: 16,
-    paddingVertical: 20
+    paddingVertical: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
+  textsContainer: {
+    flex: 1,
+    
+  },  
   name: {
     fontSize: 16,
   },
   subtitle: {
     marginTop: 2,
-    color: '#565656'
+    color: "#565656",
   },
   serprator: {
     height: StyleSheet.hairlineWidth,
-    backgroundColor: '#E2E2E2',
-    marginStart: 16
-  }
-})
+    backgroundColor: "#E2E2E2",
+    marginStart: 16,
+  },
+});
 
 export default Chats;

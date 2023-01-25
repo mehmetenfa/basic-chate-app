ad112e3d8f25b79113da38d32c4b42a182b1e07d
qxan <mehmet.enfa@gmail.com>
2023-01-25 17:40:30 +0300

    .

--- screens/Chats.js
+++ screens/Chats.js
@@ -1,13 +1,5 @@
 import React from "react";
-import {
-  View,
-  Text,
-  SafeAreaView,
-  TouchableOpacity,
-  StyleSheet,
-  TextBase,
-} from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { View, SafeAreaView, StyleSheet } from "react-native";
 import ContactRow from "../components/ContactRow";
 
 const Chats = () => {
@@ -21,35 +13,6 @@ const Chats = () => {
 };
 
 const styles = StyleSheet.create({
-  row: {
-    paddingHorizontal: 16,
-    paddingVertical: 20,
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  avatar: {
-    width: 56,
-    height: 56,
-    backgroundColor: "#2196f3",
-    borderRadius: 28,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  avatarLabel: {
-    fontSize: 20,
-    color: "white",
-  },
-  textsContainer: {
-    flex: 1,
-    marginStart: 16,
-  },
-  name: {
-    fontSize: 16,
-  },
-  subtitle: {
-    marginTop: 2,
-    color: "#565656",
-  },
   serprator: {
     height: StyleSheet.hairlineWidth,
     backgroundColor: "#E2E2E2",

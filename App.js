1497de1cdb7ffc6c01b42c1a4759403aff7226f6
qxan <mehmet.enfa@gmail.com>
2023-01-27 22:02:14 +0300

    buttons add ionic

--- App.js
+++ App.js
@@ -1,18 +1,14 @@
 import React from 'react'
-import { SafeAreaView, Text, Button } from 'react-native'
-import { createStackNavigator } from '@react-navigation/stack'
+import { SafeAreaView, Text } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native'
+import {Ionicons} from '@expo/vector-icons'
 
 
 const Home = ({navigation}) => {
   return (
     <SafeAreaView>
       <Text>Hi Home Screen</Text>
-      <Button title='Go to Settings Screen'
-      onPress={() => {
-        navigation.push("Settings")
-      }}
-      />
     </SafeAreaView>
   )
 }
@@ -21,26 +17,38 @@ const Settings = ({navigation}) => {
   return (
     <SafeAreaView>
       <Text>Hi Settings Screen</Text>
-      <Button title='Go to Home Screen'
-      onPress={() => {
-        navigation.push("Home")
-      }}
-      />
     </SafeAreaView>
   )
 }
 
-const MainStack = createStackNavigator()
+const Tabs = createBottomTabNavigator()
 
 const App = () => {
   return (
     <NavigationContainer>
-      <MainStack.Navigator>
-        <MainStack.Screen name='Home' component={Home} />
-        <MainStack.Screen name='Settings' component={Settings} />
-      </MainStack.Navigator>
+      <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused
+              ? 'chatbubbles'
+              : 'chatbubbles-outline';
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'settings' : 'settings-outline';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      >
+        <Tabs.Screen name='Home' component={Home} />
+        <Tabs.Screen name='Settings' component={Settings} />
+      </Tabs.Navigator>
     </NavigationContainer>
   )
 }
 
-export default App
+export default App;

e81ff2f657e57079346e73bd1fefed9a4b5e711b
qxan <mehmet.enfa@gmail.com>
2023-01-27 21:33:36 +0300

    stack and native add

--- App.js
+++ App.js
@@ -1,9 +1,35 @@
 import React from 'react'
-import Chats from "./screens/Chats";
+import { SafeAreaView, Text } from 'react-native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+
+
+const Home = ({navigation}) => {
+  return (
+    <SafeAreaView>
+      <Text>Hi Home Screen</Text>
+    </SafeAreaView>
+  )
+}
+
+const Settings = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hi Settings Screen</Text>
+    </SafeAreaView>
+  )
+}
+
+const MainStack = createStackNavigator()
 
 const App = () => {
   return (
-    <Chats />
+    <NavigationContainer>
+      <MainStack.Navigator>
+        <MainStack.Screen name='Home' component={Home} />
+        <MainStack.Screen name='Settings' component={Settings} />
+      </MainStack.Navigator>
+    </NavigationContainer>
   )
 }
 

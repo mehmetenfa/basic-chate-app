2666b900d7c7d0530bcc1e8ec2ffd15e3a3b1b29
qxan <mehmet.enfa@gmail.com>
2023-01-29 15:16:36 +0300

    sign up buttons

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,16 +1,80 @@
 import React from "react";
-import { Text, SafeAreaView, StyleSheet } from "react-native";
+import {
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { colors } from "../config/constants";
 
 const SignUp = () => {
   return (
-    <SafeAreaView>
-      <Text>Hi Whats up</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <SafeAreaView>
+        <View style={styles.contentContainer}>
+          <Text style={styles.title}>Create new account</Text>
+          <TextInput style={styles.input} placeholder="Enter your name" />
+          <TextInput style={styles.input} placeholder="Enter your email" />
+          <TextInput style={styles.input} placeholder="Enter your password" />
+
+          <View style={styles.buttonsContainer}>
+            <TouchableOpacity style={styles.buttonContainer}>
+              <Text style={styles.buttonLabel}>Sign In</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.buttonContainer}>
+              <Text style={styles.buttonLabel}>Sign Up</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </SafeAreaView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-    
-})
+  //   margintop: {
+  //     marginTop: 35,
+  //   },
+  container: {
+    marginTop: 35,
+    flex: 1,
+    backgroundColor: colors.primary,
+  },
+  contentContainer: {
+    padding: 32,
+  },
+  title: {
+    fontSize: 36,
+    color: "white",
+    fontWeight: "800",
+    marginBottom: 16,
+  },
+  input: {
+    backgroundColor: "white",
+    fontSize: 15,
+    marginTop: 16,
+    paddingHorizontal: 16,
+    paddingVertical: 14,
+    borderRadius: 6,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 32,
+  },
+  buttonContainer: {
+    backgroundColor: "black",
+    paddingHorizontal:  16,
+    paddingVertical: 12,
+    borderRadius: 6,
+  },
+  buttonLabel: {
+    color: "white",
+    fontSize: 18,
+  },
+});
 
 export default SignUp;
